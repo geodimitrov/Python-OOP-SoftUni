32751e7de884c093227ea15514469eb538d9bfaa
geodimitrov <geo.dimitrov@yahoo.com>
2021-03-13 17:46:37 +0200

    Adding solutions

--- Classes-Instances/Exercises/07. library/project/library.py
+++ Classes-Instances/Exercises/07. library/project/library.py
@@ -1,37 +1,36 @@
 class Library:
     def __init__(self):
-        self.user_records = []
-        self.books_available = {} # {author: [book1, book2, ...]}
-        self.rented_books = {}  # {username: {bookname: days_to_return}}
+        self.user_records = []     # will store User objects
+        self.books_available = {}  # author: [book1, book2...]
+        self.rented_books = {}     # username: {book_name: days_to_return}
 
-    def get_user_obj(self, user_id):
-        user_obj = [user for user in self.user_records if user.user_id == user_id]
+    @staticmethod
+    def get_user_obj(user_id, user_records):
+        user_obj = [user for user in user_records if user.user_id == user_id]
         if user_obj:
             return user_obj[0]
-        return None
 
     def add_user(self, user):
-        if user not in self.user_records:
-            self.user_records.append(user)
-        return f"User with id = {user.user_id} already registered in the library!"
+        if user in self.user_records:
+            return f"User with id = {user.user_id} already registered in the library!"
+        self.user_records.append(user)
 
     def remove_user(self, user):
-        if user in self.user_records:
-            self.user_records.remove(user)
-            if user.username in self.rented_books:
-                self.rented_books.pop(user.username)
-            return
-        return "We could not find such user to remove!"
+        if user not in self.user_records:
+            return "We could not find such user to remove!"
+        if user.username in self.rented_books:
+            self.rented_books.pop(user.username)
+        self.user_records.remove(user)
+
 
     def change_username(self, user_id, new_username):
-        user = self.get_user_obj(user_id)
-        if not user:
-            return f"There is no user with id = {user_id}!"
-
-        if user.username == new_username:
-            return "Please check again the provided username - \
-it should be different than the username used so far!"
-        if user.username in self.rented_books:
-            self.rented_books[new_username] = self.rented_books.pop(user.username)
-        user.username = new_username
-        return f"Username successfully changed to: {new_username} for userid: {user_id}"
+        user = self.get_user_obj(user_id, self.user_records)
+        if user and not user.username == new_username:
+            if user.username in self.rented_books:
+                self.rented_books[new_username] = self.rented_books.pop(user.username)
+            user.username = new_username
+            return f"Username successfully changed to: {new_username} for userid: {user_id}"
+        if user and user.username == new_username:
+            return "Please check again the provided username - it should be \
+different than the username used so far!"
+        return f"There is no user with id = {user_id}!"
--- Classes-Instances/Exercises/07. library/project/user.py
+++ Classes-Instances/Exercises/07. library/project/user.py
@@ -5,30 +5,27 @@ class User:
         self.books = []
 
     def __repr__(self):
-        return f"{self.user_id}, {self.username}, {self.info()}"
-
-    @staticmethod
-    def is_rented(book_name, library):
-        rented_books = [book for book in library.rented_books.values()]
-        return book_name in rented_books
+        return f"{self.user_id}, {self.username}, {self.books}"
 
     def get_book(self, author, book_name, days_to_return, library):
         if book_name in library.books_available[author]:
             self.books.append(book_name)
             library.books_available[author].remove(book_name)
-            library.rented_books[self.username] = {book_name: days_to_return}
+            if self.username not in library.rented_books:
+                library.rented_books[self.username] = {}
+            library.rented_books[self.username][book_name] = days_to_return
             return f"{book_name} successfully rented for the next {days_to_return} days!"
-        if self.is_rented(book_name, library):
-            return f'The book "{book_name}" is already rented and will be \
-# available in {book_name} days!'
+
+        if self.username in library.rented_books and book_name in library.rented_books[self.username]:
+            return f'The book "{book_name}" is already rented and will be available in \
+{days_to_return} days!'
 
     def return_book(self, author, book_name, library):
         if book_name not in self.books:
             return f"{self.username} doesn't have this book in his/her records!"
         self.books.remove(book_name)
         library.books_available[author].append(book_name)
-        library.rented_books[self.username].pop(book_name)
+        del library.rented_books[self.username][book_name]
 
     def info(self):
-        return list(sorted(self.books, reverse=True))
-
+        return ", ".join(sorted(self.books))
