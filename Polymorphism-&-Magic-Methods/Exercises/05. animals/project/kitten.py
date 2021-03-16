3396cf99fcb86cbf6f5c43a917af006414012914
geodimitrov <geo.dimitrov@yahoo.com>
2021-03-16 16:56:45 +0200

    Adding solutions

---
+++ Polymorphism-&-Magic-Methods/Exercises/05. animals/project/kitten.py
@@ -0,0 +1,10 @@
+from project.cat import Cat
+
+class Kitten(Cat):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.gender = "Female"
+
+    def make_sound(self):
+        return "Meow"
