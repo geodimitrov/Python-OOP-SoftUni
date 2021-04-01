9f5121167d6e7946a5334d90f72e1763222ebc19
geodimitrov <geo.dimitrov@yahoo.com>
2021-04-01 21:32:29 +0300

    Making changes

--- Exam-Prep/Exam_16-Aug-20/skeleton/tests/test_hardware.py
+++ Exam-Prep/Exam_16-Aug-20/skeleton/tests/test_hardware.py
@@ -1,5 +1,4 @@
 import unittest
-
 from project.hardware.hardware import Hardware
 from project.software.software import Software
 
@@ -7,37 +6,29 @@ from project.software.software import Software
 class HardwareTest(unittest.TestCase):
 
     def setUp(self):
-        self.hardware = Hardware("H", "Power", 100, 80)
+        self.hardware = Hardware("H", "Heavy", 100, 100)
 
-    def test_hardware_init__expect_correct_result_upon_initialization(self):
+    def test_hardware_init_attributes__expect_correct_values(self):
         self.assertEqual("H", self.hardware.name)
-        self.assertEqual("Power", self.hardware.type)
+        self.assertEqual("Heavy", self.hardware.type)
         self.assertEqual(100, self.hardware.capacity)
-        self.assertEqual(80, self.hardware.memory)
+        self.assertEqual(100, self.hardware.memory)
         self.assertEqual([], self.hardware.software_components)
 
-    def test_hardware_init__expect_int_values(self):
-        self.assertEqual(type(100), type(self.hardware.capacity))
-        self.assertEqual(type(80), type(self.hardware.memory))
+    def test_hardware_props__expect_correct_result(self):
+        self.assertEqual(0, self.hardware.used_memory)
+        self.assertEqual(0, self.hardware.used_capacity)
 
-    def test_hardware_install__if_not_enough_capacity__expect_exception(self):
+    def test_hardware_install__if_not_enough_space_and_capacity__expect_exception(self):
         with self.assertRaises(Exception) as ex:
             self.hardware.install(Software("S", "Light", 120, 50))
         self.assertEqual("Software cannot be installed", str(ex.exception))
 
-    def test_hardware_install__if_not_enough_memory__expect_exception(self):
-        with self.assertRaises(Exception) as ex:
-            self.hardware.install(Software("S", "Light", 80, 100))
-        self.assertEqual("Software cannot be installed", str(ex.exception))
-
-    # def test_hardware_install__if_enough_capacity_and_memory__expect_correct_result(self):
-    #     self.hardware.install(Software("S", "Light", 80, 50))
-    #     self.assertEqual(1, len(self.hardware.software_components))
-
     def test_hardware_uninstall__expect_to_remove_software(self):
-        self.hardware.install(Software("S", "Light", 80, 50))
-        self.hardware.uninstall(Software("S", "Light", 80, 50))
-        self.assertEqual(0, len(self.hardware.software_components))
+        software = Software("S", "Light", 50, 50)
+        self.hardware.install(software)
+        self.hardware.uninstall(software)
+        self.assertNotIn(software, self.hardware.software_components)
 
 if __name__ == "__main__":
     unittest.main()
