c240b53e2da67f7779424a21ccefd2ea456dbd20
geodimitrov <geo.dimitrov@yahoo.com>
2021-04-02 22:28:30 +0300

    Making changes

--- Exam-Prep/Exam_16-Aug-20/project/system.py
+++ Exam-Prep/Exam_16-Aug-20/project/system.py
@@ -78,18 +78,3 @@ class System:
     @staticmethod
     def system_split():
         return "".join(f"{hardware.__repr__()}" for hardware in System._hardware)
-
-# #
-System.register_power_hardware("HDD", 200, 200)
-System.register_heavy_hardware("SSD", 400, 400)
-# print(System.analyze())
-System.register_light_software("HDD", "Test", 0, 10)
-print(System.register_express_software("HDD", "Test2", 100, 100))
-System.register_express_software("HDD", "Test3", 50, 100)
-System.register_light_software("SSD", "Windows", 20, 50)
-System.register_express_software("SSD", "Linux", 50, 100)
-System.register_light_software("SSD", "Unix", 20, 50)
-print(System.register_light_software("FFD", "Random S", 20, 50))
-print(System.analyze())
-System.release_software_component("SSD", "Linux")
-print(System.system_split())
